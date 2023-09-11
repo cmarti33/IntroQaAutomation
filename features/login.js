4bc6f3d5e1d154318f06b5c4604098dd3fea4192
Cindy Martinez <cindy.martinez@farmu.com.co>
2023-09-10 20:00:53 -0500

    Check login, LoginOk, Login with Error and Logout

--- features/login.js
+++ features/login.js
@@ -36,7 +36,7 @@ test('Loginok', async t => {
         .typeText(inputUserName, username)
         .typeText(inputPassword, password)
         .click(buttonLogin)
-        .wait(8000);
+        .wait(4000);
 
     await t
 
@@ -48,6 +48,94 @@ test('Loginok', async t => {
 
 
 });
+
+
+test('Loginnotok', async t => {
+
+    var logo = Selector('div').withAttribute('class','login_logo');
+    var textoButton = await buttonLogin.value;
+    const username = 'standard_user5';
+    const password = 'secret_sauces5';
+
+    //declare actions
+    await t
+        .typeText(inputUserName, username)
+        .typeText(inputPassword, password)
+        .click(buttonLogin)
+        .wait(4000);
+
+    await t
+
+    .expect(logo.exists).ok('The logo is not ok')
+    .expect(inputUserName.exists).ok('The username input is not ok')
+    .expect(inputPassword.exists).ok('The password input is not ok')
+    .expect(buttonLogin.exists).ok('The buttonLogin input is not ok')
+    .expect(textoButton == 'Login').ok('The text in button is not ok');
+
+
+
+
+
+});
+
+test('Logout', async t => {
+
+    var product = Selector('span').withAttribute('class','title');
+    var buttonMenu = Selector('#react-burger-menu-btn');
+    var buttonLogout = Selector('#logout_sidebar_link');
+    var logo = Selector('div').withAttribute('class','login_logo');
+    var textoButton = await buttonLogin.value;
+    const username = 'standard_user';
+    const password = 'secret_sauce';
+//declare actions
+    await t
+        .typeText(inputUserName, username)
+        .typeText(inputPassword, password)
+        .click(buttonLogin)
+        .wait(4000);
+
+    await t
+
+        .expect(product.exists).ok('Product not exist')
+        .expect(product.withText('Products').exists).ok('Texto not exist');
+
+    await t 
+        .click(buttonMenu)
+        .click(buttonLogout)
+        .wait(4000)
+
+    await t
+    .expect(logo.exists).ok('The logo is not ok')
+    .expect(inputUserName.exists).ok('The username input is not ok')
+    .expect(inputPassword.exists).ok('The password input is not ok')
+    .expect(buttonLogin.exists).ok('The buttonLogin input is not ok')
+    .expect(textoButton == 'Login').ok('The text in button is not ok');
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+   
+    
+
+   
+
+    
+
+
+
+
 
 
 
